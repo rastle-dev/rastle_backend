540a9e6117e92b06e215cfc588ecee87b4ebd521
limyounsoo <yslim815@gmail.com>
2023-08-02 12:45:02 +0900

    feat: 회원가입 기능 추가
    회원가입 시 이메일 중복 확인을 통해 회원가입을 완료할 수 있도록 구현하였습니다.

--- src/main/java/rastle/dev/rastle_backend/domain/Member/application/MemberAuthService.java
+++ src/main/java/rastle/dev/rastle_backend/domain/Member/application/MemberAuthService.java
@@ -1,4 +1,35 @@
 package rastle.dev.rastle_backend.domain.Member.application;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import rastle.dev.rastle_backend.domain.Member.dto.MemberDTO.SignUpDto;
+import rastle.dev.rastle_backend.domain.Member.model.Member;
+import rastle.dev.rastle_backend.domain.Member.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
 public class MemberAuthService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignUpDto signUp(SignUpDto signUpDto) {
+        // 회원가입 요청 DTO의 이메일이 이미 존재하는지 확인
+        // 이걸 따로 빼는게 좋을까?
+        Optional<Member> existingMemberOptional = memberRepository.findByEmail(signUpDto.getEmail());
+        if (existingMemberOptional.isPresent()) {
+            throw new IllegalArgumentException("Email is already registered.");
+        }
+
+        signUpDto.encode(passwordEncoder);
+        Member entity = signUpDto.toEntity();
+        memberRepository.save(entity);
+
+        return signUpDto;
+    }
 }
