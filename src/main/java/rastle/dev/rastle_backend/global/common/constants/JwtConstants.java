257b8687a539d05e1d611bfa080836acd40c81b5
limyounsoo <yslim815@gmail.com>
2023-09-10 18:22:30 +0900

    test: 액세스 토큰 유지 시간 수정
    액세스 토큰 유지 시간 15초로 임시 수정

--- src/main/java/rastle/dev/rastle_backend/global/common/constants/JwtConstants.java
+++ src/main/java/rastle/dev/rastle_backend/global/common/constants/JwtConstants.java
@@ -3,8 +3,10 @@ package rastle.dev.rastle_backend.global.common.constants;
 public class JwtConstants {
     public static final String AUTHORITIES_KEY = "auth";
     public static final String BEARER_TYPE = "Bearer";
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 10;           // 10분 1000 * 60 * 10
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일 1000 * 60 * 60 * 24 * 7
+    // public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 10; // 10분
+    // 1000 * 60 * 10
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 15000; // 15초
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일 1000 * 60 * 60 * 24 * 7
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
 }
