52d4407b4c7b5d14f5625664e9a058d8b99b30be
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-03 14:41:29 +0900

    chore: yml 파일 구조 변경

--- src/main/java/rastle/dev/rastle_backend/RastleBackendApplication.java
+++ src/main/java/rastle/dev/rastle_backend/RastleBackendApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableJpaAuditing
+@EnableMongoRepositories
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 //@SpringBootApplication
 public class RastleBackendApplication {
