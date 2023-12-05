003f61b4f0aa33b6a92c942659ee447ce495ce30
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-05 16:15:23 +0900

    feat: 배포 도메인 변경

--- src/main/java/rastle/dev/rastle_backend/domain/order/dto/OrderDTO.java
+++ src/main/java/rastle/dev/rastle_backend/domain/order/dto/OrderDTO.java
@@ -1,9 +1,7 @@
 package rastle.dev.rastle_backend.domain.order.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
 import java.util.List;
 
@@ -17,25 +15,34 @@ public class OrderDTO {
 
         List<ProductOrderRequest> orderProducts;
 
-        @Getter
-        @NoArgsConstructor
-        @AllArgsConstructor
-        @Builder
-        private static class ProductOrderRequest {
-            Long productId;
-            String color;
-            String size;
-            Long count;
-            Long totalPrice;
-        }
-
     }
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ProductOrderRequest {
+        @Schema(description = "상품 아이디", defaultValue = "1")
+        Long productId;
+        @Schema(description = "상품 색상", defaultValue = "BLUE")
+        String color;
+        @Schema(description = "상품 사이즈", defaultValue = "M")
+        String size;
+        @Schema(description = "상품 구매 수량", defaultValue = "2")
+        Long count;
+        @Schema(description = "상품 총 구매 금액", defaultValue = "60000 // 상품 1개 금액 X 상품 구매 수량")
+        Long totalPrice;
+    }
 
     @Getter
+    @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class OrderCreateResponse {
+        String productOrderId;
+        @Schema(description = "주문 번호", defaultValue = "20231201T1010001")
+        String orderNumber;
 
+        List<ProductOrderRequest> orderProducts;
     }
 }
---
+++ src/main/java/rastle/dev/rastle_backend/global/component/OrderNumberComponent.java
@@ -0,0 +1,9 @@
+package rastle.dev.rastle_backend.global.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class OrderNumberComponent {
+}
