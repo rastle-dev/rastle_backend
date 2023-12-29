b1d724d843921efe3af5ee46d3361853a90c15e2
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-29 14:38:58 +0900

    feat: 결제 사전 검증 빈 틀 생성

--- src/main/java/rastle/dev/rastle_backend/domain/payment/api/PaymentController.java
+++ src/main/java/rastle/dev/rastle_backend/domain/payment/api/PaymentController.java
@@ -23,10 +23,17 @@ import rastle.dev.rastle_backend.global.response.ServerResponse;
 @RequestMapping("/payments")
 public class PaymentController {
     private final PaymentService paymentService;
-    @Operation(summary = "결제 검증 및 생성 API", description = "포트원 API에서 발생한 결제 요청을 검증하고, memberOrder를 생성하는 API입니다.")
+
+    @Operation(summary = "결제 사후 검증 및 생성 API", description = "포트원 API에서 발생한 결제 요청을 검증하고, memberOrder를 생성하는 API입니다.")
     @ApiResponse(responseCode = "200", description = "검증 성공시", content = @Content(schema = @Schema(implementation = PaymentVerificationResponse.class)))
     @PostMapping("/complete")
-    public ResponseEntity<?> verifyPayment(@RequestBody PaymentVerificationRequest paymentVerificationRequest) {
+    public ResponseEntity<?> verifyPaymentCompletion(@RequestBody PaymentVerificationRequest paymentVerificationRequest) {
         return ResponseEntity.ok(new ServerResponse<>(paymentService.verifyPayment(paymentVerificationRequest)));
+    }
+
+    @Operation(summary = "결제 사전 검증 API", description = "결제 사전 검증 API 입니다.")
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyPaymentCreation() {
+        return null;
     }
 }
