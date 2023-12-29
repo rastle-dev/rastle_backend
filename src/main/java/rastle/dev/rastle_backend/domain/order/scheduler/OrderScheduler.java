042f50930a0e58fed8bc9703bd8aaeb826b675b5
limyounsoo <yslim815@gmail.com>
2023-12-29 23:00:40 +0900

    feat: paymentStatus가 READY인 주문서에 대해 매주 월요일 삭제하는 스케줄러 추가

---
+++ src/main/java/rastle/dev/rastle_backend/domain/order/scheduler/OrderScheduler.java
@@ -0,0 +1,19 @@
+package rastle.dev.rastle_backend.domain.order.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import rastle.dev.rastle_backend.domain.order.repository.mysql.OrderDetailRepository;
+
+@Component
+@RequiredArgsConstructor
+public class OrderScheduler {
+    private final OrderDetailRepository orderDetailRepository;
+
+    @Scheduled(cron = "0 0 0 * * MON") // 매주 월요일 자정에 실행
+    // @Scheduled(cron = "0 * * * * ?") // 매 분 0초에 실행
+    public void deleteReadyOrders() {
+        orderDetailRepository.deleteAllReadyOrders();
+    }
+}
