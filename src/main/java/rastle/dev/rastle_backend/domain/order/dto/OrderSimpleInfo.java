d29640cac6286d698549c198429a6cdbbd1308dc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-12 23:55:02 +0900

    bugfix : 코드 오류 수정

--- src/main/java/rastle/dev/rastle_backend/domain/order/dto/OrderSimpleInfo.java
+++ src/main/java/rastle/dev/rastle_backend/domain/order/dto/OrderSimpleInfo.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class OrderSimpleInfo {
-    Long orderId,
+    Long orderId;
     LocalDateTime orderDate;
     String orderNumber;
     DeliveryStatus deliveryStatus;
