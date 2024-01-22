81c0d8fa7eac368ef655fef4ef2e11b242b096df
limyounsoo <yslim815@gmail.com>
2024-01-22 16:42:11 +0900

    test: 이벤트 상품 응모 관련 기능 추가

--- src/main/java/rastle/dev/rastle_backend/domain/event/model/EventProductApply.java
+++ src/main/java/rastle/dev/rastle_backend/domain/event/model/EventProductApply.java
@@ -1,5 +1,9 @@
 package rastle.dev.rastle_backend.domain.event.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,9 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import rastle.dev.rastle_backend.domain.member.model.Member;
+import rastle.dev.rastle_backend.domain.product.model.ProductBase;
 
 @Entity
 @Getter
@@ -26,5 +32,25 @@ public class EventProductApply {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private Long product_id;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "instagram_id")
+    private String instagramId;
+
+    @CreatedDate
+    @Column(name = "apply_date", updatable = false)
+    private LocalDateTime applyDate;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductBase eventApplyProduct;
+
+    @Builder
+    public EventProductApply(Member member, String phoneNumber, String instagramId, ProductBase eventApplyProduct) {
+        this.member = member;
+        this.phoneNumber = phoneNumber;
+        this.instagramId = instagramId;
+        this.eventApplyProduct = eventApplyProduct;
+    }
 }
--- src/main/java/rastle/dev/rastle_backend/domain/event/repository/mysql/EventProductApplyRepository.java
+++ src/main/java/rastle/dev/rastle_backend/domain/event/repository/mysql/EventProductApplyRepository.java
@@ -1,7 +1,19 @@
 package rastle.dev.rastle_backend.domain.event.repository.mysql;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import rastle.dev.rastle_backend.domain.event.dto.EventProductApplyDTO.EventProductApplyInfoDTO;
 import rastle.dev.rastle_backend.domain.event.model.EventProductApply;
 
 public interface EventProductApplyRepository extends JpaRepository<EventProductApply, Long> {
+    @Query("SELECT new rastle.dev.rastle_backend.domain.event.dto.EventProductApplyDTO$EventProductApplyInfoDTO(" +
+            "pb.name, epa.applyDate, epa.instagramId, epa.phoneNumber) " +
+            "FROM EventProductApply epa " +
+            "JOIN epa.product_id pb " +
+            "WHERE epa.member.id = :memberId")
+    List<EventProductApplyInfoDTO> getEventProductApplyInfoDTOs(@Param("memberId") Long memberId);
 }
