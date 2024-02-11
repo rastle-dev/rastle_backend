b773941452cac4e157e045357c6c8aa46e05ad83
limyounsoo <yslim815@gmail.com>
2024-02-11 12:52:39 +0900

    test: JwtConstants 값 변경
    access token, refresh token의 만료시간을 각 10초, 20초로 변경

--- src/main/java/rastle/dev/rastle_backend/global/common/constants/JwtConstants.java
+++ src/main/java/rastle/dev/rastle_backend/global/common/constants/JwtConstants.java
@@ -4,9 +4,13 @@ public class JwtConstants {
     public static final String AUTHORITIES_KEY = "auth";
     public static final String BEARER_TYPE = "Bearer";
     // public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 10; // 10분
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일 1000 * 60 * 60 * 24 * 7 // 일주일
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 10; // 10초
+    // public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
+    // // 일주일
 
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일 1000 * 60 * 60 * 24 * 7 // 일주일
+    // public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
+    // // 일주일
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 20; // 20초
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
