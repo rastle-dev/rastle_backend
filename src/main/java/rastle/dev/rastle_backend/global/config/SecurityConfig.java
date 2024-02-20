b7fef5773dd2af78318f35bfdab15a1ec48e8cca
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-02-20 17:58:01 +0900

    feat : security cors 도메인 추가

--- src/main/java/rastle/dev/rastle_backend/global/config/SecurityConfig.java
+++ src/main/java/rastle/dev/rastle_backend/global/config/SecurityConfig.java
@@ -128,7 +128,7 @@ public class SecurityConfig {
 
                 configuration.setAllowedOrigins(
                                 Arrays.asList("http://localhost:3000", "https://localhost:3000",
-                                                "https://www.recordyslow.com", "https://recordyslow.com", "https://api.recordyslow.com"));
+                                                "https://www.recordyslow.com", "https://recordyslow.com", "https://api.recordyslow.com", "https://optimize.d2fyka5dusxft6.amplifyapp.com"));
                 configuration.setAllowedMethods(
                                 Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH"));
                 configuration.addAllowedHeader(("*"));
