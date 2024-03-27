c37bebc1619d24505e277c7860e128c247df1ebd
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-03-27 09:40:21 +0900

    feat : init DeliveryTrackerComponent

---
+++ src/main/java/rastle/dev/rastle_backend/global/component/DeliveryTracker.java
@@ -0,0 +1,35 @@
+package rastle.dev.rastle_backend.global.component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import static java.util.Collections.singletonList;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Component
+@RequiredArgsConstructor
+public class DeliveryTracker {
+    @Value("delivery_tracker.id")
+    private String id;
+    @Value("delivery_tracker.secret")
+    private String secret;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final String BASE_URL = "https://auth.tracker.delivery";
+    private final String TOKEN_URI = "/oauth2/token";
+
+
+    private String getAccessToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(singletonList(APPLICATION_JSON));
+        headers.setContentType(APPLICATION_FORM_URLENCODED);
+    }
+
+
+}
