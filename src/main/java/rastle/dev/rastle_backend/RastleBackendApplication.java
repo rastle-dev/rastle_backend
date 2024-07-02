0e06ae937aa1178fd2fd197161fa385b4e813741
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-07-02 11:03:52 +0900

    feat : 사전검증 요청시 응답 값 세분화

--- src/main/java/rastle/dev/rastle_backend/RastleBackendApplication.java
+++ src/main/java/rastle/dev/rastle_backend/RastleBackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @EnableScheduling
 @EnableCaching
+@EnableAsync
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RastleBackendApplication {
 
