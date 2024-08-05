d38eb6042726c877f50986e49481e112f7270a37
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-08-05 15:51:03 +0900

    unload unused import

--- src/main/java/rastle/dev/rastle_backend/global/config/RedisConfig.java
+++ src/main/java/rastle/dev/rastle_backend/global/config/RedisConfig.java
@@ -1,22 +1,15 @@
 package rastle.dev.rastle_backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import rastle.dev.rastle_backend.global.cache.ObjectRedisTemplate;
 import rastle.dev.rastle_backend.global.cache.StringRedisTemplate;
-import rastle.dev.rastle_backend.global.common.constants.CacheConstant;
-
-import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
